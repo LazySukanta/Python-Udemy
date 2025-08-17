11da27b2082f401b5776f313a5af5e5f461dc8cf
Sukanta <dindasukanta19@gmail.com>
2025-08-17 23:55:31 +0530

    optimised digit count

--- DSA/basic.py
+++ DSA/basic.py
@@ -1,8 +1,16 @@
+'''
 def countDigit(n):
   a = 0
   while n>0:
     n = n//10
     a = a + 1
   print(a)
+countDigit(14)
+'''
 
-countDigit(700)
+import math
+def countDigit(n):
+  if n == 0:
+    return 1
+  return int(math.log10(n) + 1)
+print(countDigit(1110))
